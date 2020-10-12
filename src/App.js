ad33ee13f4b7e0d0455324d8eb5d1778b45b0faf
AceIsHuman <menoman1995@yahoo.com>
2020-10-11 22:57:15 -0500

    add footer and text to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Our Pricing
+      Annually
+      Monthly
+
+      Basic
+      &dollar;19.99
+      &dollar;199.99
+      500 GB Storage
+      2 Users Allowed
+      Send up to 3 GB
+      Learn More
+
+      Professional
+      &dollar;24.99
+      &dollar;249.99
+      1 TB Storage
+      5 Users Allowed
+      Send up to 10 GB
+      Learn More
+
+      Master
+      &dollar;39.99
+      &dollar;399.99
+      2 TB Storage
+      10 Users Allowed
+      Send up to 20 GB
+      Learn More
+
+      <Footer />
     </div>
   );
 }
