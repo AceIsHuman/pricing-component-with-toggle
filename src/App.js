10311ef791493b017221cf69095fdad1b4146934
AceIsHuman <menoman1995@yahoo.com>
2020-10-17 21:28:37 -0500

    import PricingCard

--- src/App.js
+++ src/App.js
@@ -3,35 +3,40 @@ import styled from 'styled-components';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PricingCard from './components/PricingCard';
 
 function App() {
   return (
     <AppContainer>
       <Header />
-      Basic
-      &dollar;19.99
-      &dollar;199.99
-      500 GB Storage
-      2 Users Allowed
-      Send up to 3 GB
-      Learn More
-
-      Professional
-      &dollar;24.99
-      &dollar;249.99
-      1 TB Storage
-      5 Users Allowed
-      Send up to 10 GB
-      Learn More
-
-      Master
-      &dollar;39.99
-      &dollar;399.99
-      2 TB Storage
-      10 Users Allowed
-      Send up to 20 GB
-      Learn More
-
+      <CardContainer>
+        <PricingCard
+          tier="Basic"
+          monthlyPrice="19.99"
+          annualPrice="199.99"
+          storage="500 GB Storage"
+          users="2 Users Allowed"
+          limit="Send up to 3 GB"
+        />
+        <PricingCard
+          tier="Professional"
+          monthlyPrice="24.99"
+          annualPrice="249.99"
+          storage="1 TB Storage"
+          users="5 Users Allowed"
+          limit="Send up to 10 GB"
+          background="rgb(151,156,237)"
+          centerCard
+        />
+        <PricingCard
+          tier="Master"
+          monthlyPrice="39.99"
+          annualPrice="399.99"
+          storage="2 TB Storage"
+          users="10 Users Allowed"
+          limit="Send up to 20 GB"
+        />
+      </CardContainer>
       <Footer />
     </AppContainer>
   );
@@ -46,6 +51,13 @@ const AppContainer = styled.div`
   text-align: center;
   flex-direction: column;
   font-family: sans-serif;
-`
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+`;
 
 export default App;
