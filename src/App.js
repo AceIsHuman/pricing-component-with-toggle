cee0a497d44a55a1b7d7d020c0d6968d89f59368
AceIsHuman <menoman1995@yahoo.com>
2020-10-17 22:06:17 -0500

    refactor to class component and add toggled state and handler, pass toggled state to pricing cards

--- src/App.js
+++ src/App.js
@@ -5,41 +5,58 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import PricingCard from './components/PricingCard';
 
-function App() {
-  return (
-    <AppContainer>
-      <Header />
-      <CardContainer>
-        <PricingCard
-          tier="Basic"
-          monthlyPrice="19.99"
-          annualPrice="199.99"
-          storage="500 GB Storage"
-          users="2 Users Allowed"
-          limit="Send up to 3 GB"
-        />
-        <PricingCard
-          tier="Professional"
-          monthlyPrice="24.99"
-          annualPrice="249.99"
-          storage="1 TB Storage"
-          users="5 Users Allowed"
-          limit="Send up to 10 GB"
-          background="rgb(151,156,237)"
-          centerCard
-        />
-        <PricingCard
-          tier="Master"
-          monthlyPrice="39.99"
-          annualPrice="399.99"
-          storage="2 TB Storage"
-          users="10 Users Allowed"
-          limit="Send up to 20 GB"
-        />
-      </CardContainer>
-      <Footer />
-    </AppContainer>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { toggled: true };
+
+    // allow `this` to work in the callback
+    this.setToggled = this.setToggled.bind(this);
+  }
+
+  setToggled() {
+    this.setState((state) => ({ toggled: !state.toggled }));
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <Header toggled={this.state.toggled} setToggled={this.setToggled} />
+        <CardContainer>
+          <PricingCard
+            tier="Basic"
+            monthlyPrice="19.99"
+            annualPrice="199.99"
+            storage="500 GB Storage"
+            users="2 Users Allowed"
+            limit="Send up to 3 GB"
+            toggled={this.state.toggled}
+          />
+          <PricingCard
+            tier="Professional"
+            monthlyPrice="24.99"
+            annualPrice="249.99"
+            storage="1 TB Storage"
+            users="5 Users Allowed"
+            limit="Send up to 10 GB"
+            background="rgb(151,156,237)"
+            centerCard
+            toggled={this.state.toggled}
+          />
+          <PricingCard
+            tier="Master"
+            monthlyPrice="39.99"
+            annualPrice="399.99"
+            storage="2 TB Storage"
+            users="10 Users Allowed"
+            limit="Send up to 20 GB"
+            toggled={this.state.toggled}
+          />
+        </CardContainer>
+        <Footer />
+      </AppContainer>
+    );
+  }
 }
 
 const AppContainer = styled.div`
