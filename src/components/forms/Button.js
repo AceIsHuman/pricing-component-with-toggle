1bdf58eab4a3b2ff5e693b6e249c2443d1cd5702
AceIsHuman <menoman1995@yahoo.com>
2020-11-01 22:45:22 -0600

    update default background to gradient

--- src/components/forms/Button.js
+++ src/components/forms/Button.js
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 const Button = styled.button`
   width: ${(props) => props.width || '100%'};
   margin: ${(props) => props.margin || '0.5rem'};
-  padding: 0.8rem;
-  background-color: ${(props) => props.background || 'rgb(151,156,237)'};
+  padding: 0.6rem;
+  background: ${(props) =>
+    props.background ||
+    'linear-gradient(to right, rgb(151,156,237), rgb(101,106,187))'};
   color: ${(props) => props.color || '#ffffff'};
-  border-radius: 0.2rem;
+  border-radius: 0.4rem;
+  font-variant: all-petite-caps;
 `;
 
 export default Button;
